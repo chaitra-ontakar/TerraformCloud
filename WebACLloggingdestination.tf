3cc72592891d30b1c6b936ac5ee976f647f2a0c1
chaitra-ontakar <chaitra.ontakar@gds.ey.com>
2023-06-14 15:30:43 +0530

    S3 Bucket

--- WebACLloggingdestination.tf
+++ WebACLloggingdestination.tf
@@ -1,5 +1,5 @@
 resource "aws_wafv2_web_acl_logging_configuration" "weblogging" {
-  log_destination_configs = [aws_kinesis_firehose_delivery_stream.extended_s3_stream.arn]
+  log_destination_configs = [aws_s3_bucket.Bucket]
   resource_arn            = aws_wafv2_web_acl.webacl.arn
   redacted_fields {
     single_header {
