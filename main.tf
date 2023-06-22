4f32abcdad77b752678fe61e3fc00c5aa453aa1d
chaitra-ontakar <chaitra.ontakar@gds.ey.com>
2023-06-22 13:50:47 +0530

    third commit

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_launch_template" "Template" {
   instance_type = "t2.micro"
  
   network_interfaces {
-    associate_public_ip_address = true
+    associate_public_ip_address = false
   }
 
 }
