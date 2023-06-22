55c8c668d8d042a9b68a3aa9a56b9b9357901fc2
chaitra-ontakar <chaitra.ontakar@gds.ey.com>
2023-06-22 18:01:21 +0530

    firstcommit

--- provider.tf
+++ provider.tf
@@ -9,4 +9,7 @@ terraform {
 
 provider "aws" {
   # Configuration options
+  access_key = "AKIAUZD7U647AZ2U6F4U"
+  secret_key = "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"
+  region = "us-east-2"
       }
