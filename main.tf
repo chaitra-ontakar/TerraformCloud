8bd0117886cd281f4dc6e6d544995768fff115f0
chaitra-ontakar <chaitra.ontakar@gds.ey.com>
2023-11-24 13:51:33 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
+}
+
+resource "aws_sns_topic_subscription" "example" {
+  topic_arn         = aws_sns_topic.user_updates.arn
+  protocol          = "email"
+  endpoint          = "test@example.com"
+  endpoint_auto_confirms = false
+}
