e165eff9baa006be66acc0d4e562d7e4ac483858
chaitra-ontakar <chaitra.ontakar@gds.ey.com>
2024-01-11 17:46:55 +0530

    3

--- main.tf
+++ main.tf
@@ -54,9 +54,4 @@ resource "aws_iam_role" "firehose_role" {
 resource "aws_wafv2_web_acl_logging_configuration" "loggingconfiguration" {
   log_destination_configs = [aws_kinesis_firehose_delivery_stream.extended_s3_stream.arn]
   resource_arn            = aws_wafv2_web_acl.webacl.arn
-  redacted_fields {
-    single_header {
-      name = "token"
-    }
-  }
 }
